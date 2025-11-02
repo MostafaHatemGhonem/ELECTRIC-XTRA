4e9725b22ffcb6486c9d68d1b2fa2b54feb9b3f4
MostafaHatemGhonem <hatmmstfy536@gmail.com>
2025-11-02 03:28:55 +0200

    add Header and Background for Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function BackgroundPage() {
+  return (
+    <div className='bgc'>
+      <div className="circle"></div>
+      <div className='square'></div>
+      <div className='triangle'></div>
+      <div className='fullSquare'></div>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="logo">
+        ELECTRIC XTRA
+      </div>
+      <div className="links">
+        <ul>
+          <a href="#home">
+            <li>Home</li>
+          </a>
+          <a href="#features">
+            <li>Features</li>
+          </a>
+          <a href="#about">
+            <li>About</li>
+          </a>
+          <a href="#contact">
+            <li>Contact</li>
+          </a>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BackgroundPage/>
+    <Header/>
+    </>
+  )
 }
 
 export default App;
